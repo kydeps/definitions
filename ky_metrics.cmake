e1126c3dd29ba7f023e55dd5d15681173a5b7f5b
Kamen Yotov <kamen@yotov.org>
2022-02-05 23:00:49 -0500

    add ky_metrics

---
+++ ky_metrics.cmake
@@ -0,0 +1,6 @@
+include_guard(GLOBAL)
+
+KyDep(ky_metrics
+    GIT_REPOSITORY https://github.com/kydeps/ky_metrics.git
+    GIT_TAG 8b58065b48d0e04cf688679b2f164c7f1ed42b50 # main as of 2022-02-06
+)
